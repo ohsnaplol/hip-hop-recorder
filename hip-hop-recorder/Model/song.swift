70c2fa13413dac01437272e6f335ebca5c3539ac
ohsnaplol <emyb@bellsouth.net>
2018-07-31 01:51:31 +0200

    initial upload

---
+++ hip-hop-recorder/Model/song.swift
@@ -0,0 +1,18 @@
+//
+//  song.swift
+//  hip-hop-recorder
+//
+//  Created by Marcello Galvan on 7/19/18.
+//  Copyright © 2018 Marcello Galvan. All rights reserved.
+//
+
+import Foundation
+import CoreAudio
+
+class Song {
+    let title: String = ""
+    let artist: String = ""
+    let beatArtist: String = ""
+//    let userTrack: AudioBuffer
+//    let backgroundTrack: AudioBuffer
+}
---
+++ hip-hop-recorder/Views/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  hip-hop-recorder
+//
+//  Created by Marcello Galvan on 7/16/18.
+//  Copyright © 2018 Marcello Galvan. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioRecorderDelegate  {
+    
+    @IBOutlet weak var recordButton: UIButton!
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        fillTable(collection: <#T##Array<Any>#>)
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        
+                    } else {
+                        // failed to record!
+                    }
+                }
+            }
+        } catch {
+            // failed to record!
+        }
+    }
+    
+    func startRecording() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            
+            recordButton.setTitle("Tap to Stop", for: .normal)
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        
+        if success {
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+        } else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+            // recording failed :(
+        }
+    }
+    
+    @objc func recordTapped() {
+        if audioRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func fillTable(collection: Array<Any>) {
+//        Here we fill the table with previous recordings
+//        recordingsCollection.
+    }
+
+
+}
+
